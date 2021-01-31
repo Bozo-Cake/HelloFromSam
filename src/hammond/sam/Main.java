41d46337a308b6a7598dfd89f7616c12faa51830
Bozo-Cake <78047932+Bozo-Cake@users.noreply.github.com>
2021-01-30 21:03:59 -0700

    Sam's after push

--- src/hammond/sam/Main.java
+++ src/hammond/sam/Main.java
@@ -3,8 +3,8 @@ package hammond.sam;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello from Sam!");
         System.out.println("Hello from Bradley!");
+        System.out.println("Sam's Confirmed Completed");
     }
 }
