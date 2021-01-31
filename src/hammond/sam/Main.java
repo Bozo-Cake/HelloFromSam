0a6c2a41d169cd7e615ad7f19b3ebc0467679770
bgardner-edu <gar19078@byui.edu>
2021-01-30 20:43:21 -0700

    Hi from Bradley

--- src/hammond/sam/Main.java
+++ src/hammond/sam/Main.java
@@ -3,6 +3,8 @@ package hammond.sam;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello from Sam!");
+
+        System.out.println("Hello from Sam!");
+        System.out.println("Hello from Bradley!");
     }
 }
